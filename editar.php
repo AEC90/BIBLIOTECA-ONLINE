6953105c6272d9eb31b5eb2df35dc3b2b91a8362
André Ezequias <51973867+Andreezequias@users.noreply.github.com>
2021-04-19 11:01:16 +0200

    Update editar.php

--- editar.php
+++ editar.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Biblioteca Online</title>
-    <link type="text/css" rel="stylesheet" href="assets/css/styles.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/styles.css?v=<?php echo time();?>">
     <script src="/assets/js/script.js"></script>
 </head>
 <?php
